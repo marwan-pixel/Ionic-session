84151ffce216b686f268d64ee483d91eb07f6bee
marwan-pixel <marwanhadid6@gmail.com>
2022-09-26 09:51:31 +0700

    Upload file

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage-angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  public name: string;
+  constructor(private storage: Storage) {
+    this.storage.get('isLoggedIn').then((val) => {
+      console.log(val);
+      this.name = val.nama;
+    });
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage-angular';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  constructor(private storage: Storage, private navCtrl: NavController) {}
 
-  constructor() {}
-
+  logout() {
+    this.storage.clear();
+    localStorage.clear();
+    this.navCtrl.navigateRoot('/login');
+  }
 }
